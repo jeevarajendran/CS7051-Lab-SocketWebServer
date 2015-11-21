b419dcff1fd239eb2742035a237fceface44c4c6
jeevarajendran <rajendrj@tcd.ie>
2015-11-21 12:19:47 +0000

    Update ClientSoc.java

--- ClientSoc.java
+++ ClientSoc.java
@@ -13,43 +13,46 @@
      *
       * @author jeevarajendran
        */
-       public class ClientSoc {
+       public class ClientSoc 
+       {
            
-	      public static void main(String[] args) {
+	      public static void main(String[] args) 
+	      {
 	              // TODO code application logic here
 		              
-			              Socket clientSoc = null;
-				              PrintStream output = null;
+			Socket clientSoc = null;
+			PrintStream output = null;
 					        
-						        try
-							            {
-								                    clientSoc = new Socket("localhost",9999);
-										                }
-												        catch(Exception e)
-													            {
-														                    System.out.println(e + "Exception in client 1 connection");
-																                }
-																		  
-																		          try
-																			              {
-																				                      output = new PrintStream(clientSoc.getOutputStream());
-																						                      BufferedReader bd = new BufferedReader(new InputStreamReader(clientSoc.getInputStream()));
-																								                      while(true)
-																										                          {
-																													                          System.out.println("Enter your message to the server : ");
-																																                          InputStreamReader rd = new InputStreamReader(System.in);
-																																			                          BufferedReader br = new BufferedReader(rd);
-																																						                          String temp = br.readLine();
-																																									                          output.println(temp);
+			try
+			{
+				clientSoc = new Socket("localhost",9999);
+			}
+			catch(Exception e)
+			{
+				System.out.println(e + "Exception in client 1 connection");
+	                }
+			  
+			try
+			{
+				output = new PrintStream(clientSoc.getOutputStream());
+				BufferedReader bd = new BufferedReader(new InputStreamReader(clientSoc.getInputStream()));
+				while(true)
+				{
+                          		System.out.println("Enter your message to the server : ");
+			        	InputStreamReader rd = new InputStreamReader(System.in);
+					BufferedReader br = new BufferedReader(rd);
+					String temp = br.readLine();
+					output.println(temp);
 																																												                          
-																																															                          String messagefromserver = bd.readLine();
-																																																		                          System.out.println("Message from Server to you : "+messagefromserver+" \n");
-																																																					                      }
-																																																							                   }
-																																																									           catch(Exception e)
-																																																										               {
-																																																											                       System.out.println(e + "Exception in client 1 message" );
-																																																													                   }
-																																																															           }  
-																																																																   }
+	                          	String messagefromserver = bd.readLine();
+					System.out.println("Message from Server to you : "+messagefromserver+" \n");
+				}
+				
+			}
+			catch(Exception e)
+			{
+	                       System.out.println(e + "Exception in client 1 message" );
+			}
+   		}  
+   	}
 
